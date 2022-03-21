4af40230ea829ddb48bd5a456bc4f22cbb77eda8
facundo <subofer@hotmail.com>
2022-03-21 19:06:47 -0300

    entrega 34

--- clase28/bash.sh
+++
@@ -1,1 +0,0 @@
-echo Ejecuta el Bash
--- clase28/processStdout.js
+++
@@ -1,7 +0,0 @@
-process.stdout.write("Holaa holaa\n")
-
-console.log = function(d) {
-    process.stdout.write("LOG: " + d + '\n')
-}
-
-console.log("Proceso corriendo...")
